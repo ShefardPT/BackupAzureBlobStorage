cfa9057239d1f5817b9cea123cb6777cec3a9763
Alexander Shefer <shefard55r@gmail.com>
2019-08-14 12:42:00 +0600

    - Added ArgumentsList Class to parse incoming arguments

---
+++ BackupAzureBlobStorage/ArgumentsList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace BackupAzureBlobStorage
+{
+    public static class ArgumentsList
+    {
+        public static void Init(string[] args)
+        {
+            var argsList = new Dictionary<string, string>()
+            {
+                { "--acckey", nameof(AccountKey) },
+                { "--accname", nameof(AccountName) }
+            };
+
+            var temp = args
+                .Select(x => x.Split('=', StringSplitOptions.RemoveEmptyEntries))
+                .Where(x => x.Length == 2)
+                .ToDictionary(x => x[0], x => x[1])
+                .Join(argsList, argsDict => argsDict.Key, x => x.Key, (argsDict, x) => argsDict);
+            
+            foreach (var item in temp)
+            {
+                var prop = typeof(ArgumentsList).GetProperty(argsList[item.Key]);
+
+                if (prop == null)
+                {
+                    continue;
+                }
+
+                prop.SetValue(null, item.Value);
+            }
+        }
+
+        public static string AccountName { get; private set; }
+        public static string AccountKey { get; private set; }
+    }
+}
---
+++ BackupAzureBlobStorage/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BackupAzureBlobStorage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArgumentsList.Init(args);
+
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+
+    }
+}
